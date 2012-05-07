5af9b583975abaecce3cd7625a8bd977aa90d776
Tobias Rojahn <master_iv@web.de>
2012-05-07 18:24:32 +0200

    fixed chackalive skript

--- checkalive.php
+++ checkalive.php
@@ -1,12 +1,8 @@
 #!/usr/bin/php5
 <?php
 
-$alive = false;
+$dir = dirname( __FILE__ ).'/';
 
-foreach( explode( "\n", `ps ax|grep "ndoe server"` ) as $line)
-	if( preg_match( '/^\s*(\d+)\s.* node server.js$/', $line, $proc ))
-		$alive = true;;
-
-if( !$alive ) {
-	`node server.js > logs/nodejs.log &`;
+if( !preg_match( "/node ".addcslashes($dir, "/")."server.js/", `ps ax|grep server` ) ) {
+	`node {$dir}server.js > {$dir}logs/nodejs.log &`;
 }
